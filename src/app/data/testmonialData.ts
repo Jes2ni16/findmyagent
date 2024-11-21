9067120b38ced514d8f9e1dda22164d449845d7d
Jestoni <jhontobit16@gmail.com>
2024-11-21 10:48:38 +0800

    as

--- src/app/data/testmonialData.ts
+++ src/app/data/testmonialData.ts
@@ -1,7 +1,6 @@
 export interface Testimonial {
     quote: string;
     name: string;
-    title: string;
     rating: number;
   }
   
@@ -9,19 +8,16 @@ export interface Testimonial {
     {
       quote: 'Through FindMyAgent, I connected with the perfect agent who helped me find the ideal rental home. They understood exactly what I was looking for and made the process so easy!',
       name: 'Jenny B.',
-      title: 'Business Owner',
       rating: 4,
     },
     {
       quote: 'FindMyAgent led me to an amazing agent who quickly found a buyer for my property. Their professionalism and dedication made all the difference!',
       name: 'Nino C.',
-      title: 'Project Manager',
       rating: 4,
     },
     {
       quote: 'Thanks to FindMyAgent, I found the perfect agent who guided me in buying a home in a housing development. Their expertise made everything smooth and hassle-free!',
       name: 'Juvy G.',
-      title: 'Freelancer',
       rating: 5,
     },
   ];
