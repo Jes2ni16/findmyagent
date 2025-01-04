b0f94e4eb4aaecbdf160ccc712b00fd350e1d461
Jes2ni16 <jhontobit16@gmail.com>
2025-01-04 14:58:49 +0800

    add list table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,6 @@ const images: Image[] = [
   { src: "/Dianne.webp", alt: "Diane", inv: "https://lookerstudio.google.com/embed/u/0/reporting/c5c9689a-ae0d-4846-859a-30238d57320f/page/p_ocwri6ymed", name: "Dianne Alvarez" },
   { src: "/Malbin.webp", alt: "Malvin Card2", inv: "https://zaiko.website/client/malvin2", name: "Malvin Sabillo" },
   { src: "/Dianne.webp", alt: "Diane", inv: "https://lookerstudio.google.com/embed/u/0/reporting/c5c9689a-ae0d-4846-859a-30238d57320f/page/p_ocwri6ymed", name: "Dianne Alvarez" },
-  { src: "/Malbin.webp", alt: "Malvin Card3", inv: "https://zaiko.website/client/malvin2", name: "Malvin Sabillo" },
   { src: "/Dianne.webp", alt: "Diane", inv: "https://lookerstudio.google.com/embed/u/0/reporting/c5c9689a-ae0d-4846-859a-30238d57320f/page/p_ocwri6ymed", name: "Dianne Alvarez" },
   { src: "/Jesua.webp", alt: "Jesua", inv: "https://zaiko.website/client/jesua1", name: "Jesua Garlet" },
   { src: "/Jesua.webp", alt: "Jesua", inv: "https://zaiko.website/client/jesua1", name: "Jesua Garlet" },
